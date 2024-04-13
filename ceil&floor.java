cf3e287a02cdd8b62d8ff38d6915634c281fc326
Heba_Mustafa🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-04-13 13:27:27 +0300

    Add files via upload

---
+++ ceil&floor.java
@@ -0,0 +1,16 @@
+package q1;
+
+import java.util.*;
+
+public class Q1 {//heba jalil
+
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+          double i = 35.67f;
+          System.out.println(Math.ceil(i));
+          System.out.println(Math.floor(i));
+     }
+
+       
+}
+
